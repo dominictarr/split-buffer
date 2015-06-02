02018a138b90ac1f1d3ac715db8ee86fdec6e523
Dominic Tarr <dominic.tarr@gmail.com>
2015-06-02 13:42:33 +0200

    initial

---
+++ index.js
@@ -0,0 +1,15 @@
+
+module.exports = function split (data, max) {
+
+  if(data.length <= max)
+    return [data]
+  var out = [], len = 0
+
+  while(len < data.length) {
+    out.push(data.slice(len, Math.min(len + max, data.length)))
+    len += max
+  }
+
+  return out
+}
+
